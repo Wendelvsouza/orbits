faf662bb827e07dd59a07a7fe5077610bbaf6c83
Wendel <wendelvaccani@gmail.com>
2024-03-27 10:44:01 -0300

    Fix: clean code

--- orbits/methods_solution/euler_runge_kutta.py
+++ orbits/methods_solution/euler_runge_kutta.py
@@ -55,6 +55,28 @@ class shortcut:
         mars["x_mars"] += mars["velocity_x_mars"] * step
         mars["y_mars"] += mars["velocity_y_mars"] * step
 
+    def update_velocity_position_2(
+        self,
+        mars,
+        earth,
+        acceleration_ax,
+        acceleration_ay,
+        acceleration_bx,
+        acceleration_by,
+        step,
+    ):
+        earth["velocity_x_earth"] = earth["velocity_x_earth0"] + acceleration_ax * step
+        earth["velocity_y_earth"] = earth["velocity_y_earth0"] + acceleration_ay * step
+
+        mars["velocity_x_mars"] = mars["velocity_x_mars0"] + acceleration_bx * step
+        mars["velocity_y_mars"] = mars["velocity_y_mars0"] + acceleration_by * step
+
+        earth["x_earth"] = earth["x_earth0"] + earth["velocity_x_earth"] * step
+        mars["x_mars"] = mars["x_mars0"] + mars["velocity_x_mars"] * step
+
+        earth["y_earth"] = earth["y_earth0"] + earth["velocity_y_earth"] * step
+        mars["y_mars"] = mars["y_mars0"] + mars["velocity_y_mars"] * step
+
     def save_positions(self, earth, mars):
 
         earth["position_earth_x"].append(earth["x_earth"])
@@ -75,7 +97,7 @@ class Orbits_euler_method(MethodsInterface):
 
     def methods(self, const, step, mars, earth) -> tuple:
         shortcut_instance = shortcut()
-        for i in range(19000):
+        for _ in range(19000):
 
             shortcut_instance.save_positions(earth, mars)
 
@@ -105,115 +127,50 @@ class Orbits_euler_method(MethodsInterface):
 class Orbits_runge_kutta_method(MethodsInterface):
 
     def methods(self, const, step, mars, earth) -> tuple:
+        shortcut_instance = shortcut()
 
         for _ in range(358000):
 
-            x_mars0 = mars["x_mars"]
-            y_mars0 = mars["y_mars"]
-            velocity_x_mars0 = mars["velocity_x_mars"]
-            velocity_y_mars0 = mars["velocity_y_mars"]
+            shortcut_instance.save_positions(earth, mars)
 
-            x_earth0 = earth["x_earth"]
-            y_earth0 = earth["y_earth"]
-            velocity_x_earth0 = earth["velocity_x_earth"]
-            velocity_y_earth0 = earth["velocity_y_earth"]
-
-            earth["new_position_earth_x"].append(earth["x_earth"])
-
-            earth["new_position_earth_y"].append(earth["y_earth"])
-
-            mars["new_position_mars_x"].append(mars["x_mars"])
-            mars["new_position_mars_y"].append(mars["y_mars"])
-
-            div_b = (mars["x_mars"] ** 2 + mars["y_mars"] ** 2) ** (3 / 2)
-            div_a = (earth["x_earth"] ** 2 + earth["y_earth"] ** 2) ** (3 / 2)
-            div_a_b = (
-                (mars["x_mars"] - earth["x_earth"]) ** 2
-                + (mars["y_mars"] - earth["y_earth"]) ** 2
-            ) ** (3 / 2)
-
-            acceleration_ax = (
-                -const["GM"] * earth["x_earth"] / div_a
-                + earth["Gm_a"] * (mars["x_mars"] - earth["x_earth"]) / div_a_b
-            )
-            acceleration_ay = (
-                -const["GM"] * earth["y_earth"] / div_a
-                + earth["Gm_a"] * (mars["y_mars"] - earth["y_earth"]) / div_a_b
+            div_a, div_b, div_a_b = shortcut_instance.div_calculator(
+                mars["x_mars"], mars["y_mars"], earth["x_earth"], earth["y_earth"]
             )
 
-            acceleration_bx = (
-                -const["GM"] * mars["x_mars"] / div_b
-                - mars["Gm_b"] * (mars["x_mars"] - earth["x_earth"]) / div_a_b
-            )
-            acceleration_by = (
-                -const["GM"] * mars["y_mars"] / div_b
-                - mars["Gm_b"] * (mars["y_mars"] - earth["y_earth"]) / div_a_b
+            acceleration_ax, acceleration_ay, acceleration_bx, acceleration_by = (
+                shortcut_instance.acceleration_calculator(
+                    const, mars, earth, div_a, div_b, div_a_b
+                )
             )
 
-            earth["velocity_x_earth"] += acceleration_ax * step["dt_runge_kutta"] / 2
-            earth["velocity_y_earth"] += acceleration_ay * step["dt_runge_kutta"] / 2
-
-            mars["velocity_x_mars"] += acceleration_bx * step["dt_runge_kutta"] / 2
-            mars["velocity_y_mars"] += acceleration_by * step["dt_runge_kutta"] / 2
-
-            earth["x_earth"] += earth["velocity_x_earth"] * step["dt_runge_kutta"] / 2
-            mars["x_mars"] += mars["velocity_x_mars"] * step["dt_runge_kutta"] / 2
-
-            earth["y_earth"] += earth["velocity_y_earth"] * step["dt_runge_kutta"] / 2
-            mars["y_mars"] += mars["velocity_y_mars"] * step["dt_runge_kutta"] / 2
-
-            div_b = (mars["x_mars"] ** 2 + mars["y_mars"] ** 2) ** (3 / 2)
-            div_a = (earth["x_earth"] ** 2 + earth["y_earth"] ** 2) ** (3 / 2)
-            div_a_b = (
-                (mars["x_mars"] - earth["x_earth"]) ** 2
-                + (mars["y_mars"] - earth["y_earth"]) ** 2
-            ) ** (3 / 2)
-
-            acceleration_ax = (
-                -const["GM"] * earth["x_earth"] / div_a
-                + earth["Gm_a"] * (mars["x_mars"] - earth["x_earth"]) / div_a_b
-            )
-            acceleration_ay = (
-                -const["GM"] * earth["y_earth"] / div_a
-                + earth["Gm_a"] * (mars["y_mars"] - earth["y_earth"]) / div_a_b
+            shortcut_instance.update_velocity_position(
+                mars,
+                earth,
+                acceleration_ax,
+                acceleration_ay,
+                acceleration_bx,
+                acceleration_by,
+                step["dt_runge_kutta"] / 2,
             )
 
-            acceleration_bx = (
-                -const["GM"] * mars["x_mars"] / div_b
-                - mars["Gm_b"] * (mars["x_mars"] - earth["x_earth"]) / div_a_b
-            )
-            acceleration_by = (
-                -const["GM"] * mars["y_mars"] / div_b
-                - mars["Gm_b"] * (mars["y_mars"] - earth["y_earth"]) / div_a_b
+            div_a, div_b, div_a_b = shortcut_instance.div_calculator(
+                mars["x_mars"], mars["y_mars"], earth["x_earth"], earth["y_earth"]
             )
 
-            earth["velocity_x_earth"] = (
-                velocity_x_earth0 + acceleration_ax * step["dt_runge_kutta"]
-            )
-            earth["velocity_y_earth"] = (
-                velocity_y_earth0 + acceleration_ay * step["dt_runge_kutta"]
-            )
-
-            mars["velocity_x_mars"] = (
-                velocity_x_mars0 + acceleration_bx * step["dt_runge_kutta"]
-            )
-            mars["velocity_y_mars"] = (
-                velocity_y_mars0 + acceleration_by * step["dt_runge_kutta"]
+            acceleration_ax, acceleration_ay, acceleration_bx, acceleration_by = (
+                shortcut_instance.acceleration_calculator(
+                    const, mars, earth, div_a, div_b, div_a_b
+                )
             )
 
-            earth["x_earth"] = (
-                x_earth0 + earth["velocity_x_earth"] * step["dt_runge_kutta"]
+            shortcut_instance.update_velocity_position_2(
+                mars,
+                earth,
+                acceleration_ax,
+                acceleration_ay,
+                acceleration_bx,
+                acceleration_by,
+                step["dt_runge_kutta"],
             )
-            mars["x_mars"] = x_mars0 + mars["velocity_x_mars"] * step["dt_runge_kutta"]
 
-            earth["y_earth"] = (
-                y_earth0 + earth["velocity_y_earth"] * step["dt_runge_kutta"]
-            )
-            mars["y_mars"] = y_mars0 + mars["velocity_y_mars"] * step["dt_runge_kutta"]
-
-        return (
-            earth["new_position_earth_x"],
-            earth["new_position_earth_y"],
-            mars["new_position_mars_x"],
-            mars["new_position_mars_y"],
-        )
+        return shortcut_instance.save_positions(earth, mars)
--- orbits/solar_system/earth_mars.py
+++ orbits/solar_system/earth_mars.py
@@ -8,18 +8,27 @@ class Orbits:
         with open("settings.json") as json_file:
             variables = json.load(json_file)
 
-        self.step = variables["step"]
-        self.earth = variables["earth"]
-        self.mars = variables["mars"]
         self.const = variables["const"]
+        self.step_euler = variables["step_euler"]
+        self.earth_euler = variables["earth_euler"]
+        self.mars_euler = variables["mars_euler"]
+
+        self.step_runge = variables["step_runge"]
+        self.earth_runge = variables["earth_runge"]
+        self.mars_runge = variables["mars_runge"]
 
     def apply_method(
         self,
         name,
         method_apply,
     ):
-        earth_x, earth_y, mars_x, mars_y = method_apply(
-            self.const, self.step, self.mars, self.earth
-        )
+        if name == "euler":
+            earth_x, earth_y, mars_x, mars_y = method_apply(
+                self.const, self.step_euler, self.mars_euler, self.earth_euler
+            )
+        else:
+            earth_x, earth_y, mars_x, mars_y = method_apply(
+                self.const, self.step_runge, self.mars_runge, self.earth_runge
+            )
         plot_euler = Plot()
         plot_euler.graph_force_grav(name, earth_x, earth_y, mars_x, mars_y)
